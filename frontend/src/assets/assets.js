0e0cbb234a304690a833b3d1eb1f5009682e2ab3
Daniel Jin <djin2001@gmail.com>
2025-07-28 21:03:17 -0600

    added tailwinCSS

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,13 @@
+import cart_icon from './cart.svg'
+import menu_icon from './menu.svg'
+import list_icon from './list.svg'
+import trash_icon from './trash.svg'
+import x_icon from './x.svg'
+
+export const assets = {
+    cart_icon,
+    menu_icon,
+    list_icon,
+    trash_icon,
+    x_icon
+}
